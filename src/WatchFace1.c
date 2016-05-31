1c300b1c36df65ddb17d9683c35bcbbb695eda42
Jeffrey <jeffrey@vanbijleveld.nl>
2016-05-31 12:08:02 +0200

    added battery percentage

--- src/WatchFace1.c
+++ src/WatchFace1.c
@@ -1,6 +1,9 @@
 #include <pebble.h>
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static int s_battery_level;
+static Layer *s_battery_layer;
+static TextLayer *s_battery_percentage;
 
 static void update_time() {
   // Get a tm structure
@@ -8,10 +11,52 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
-
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+   
   text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void battery_update_proc(Layer *layer, GContext *ctx) {
+  int size;
+  GRect bounds = layer_get_bounds(layer);
+  
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+  
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  
+  if(s_battery_level > 75){
+    if(s_battery_level == 100){
+      size = bounds.size.w;
+    }else{
+      size = (int)(float)(((float)(s_battery_level % 25) / 25.0F) * (bounds.size.w));
+    }    
+    graphics_fill_rect(ctx, GRect((bounds.size.w - size + 2), 1, (size-4), 2), 0, GCornerNone);
+    
+    graphics_fill_rect(ctx, GRect((bounds.size.w-4), 1, 2, bounds.size.h-8), 0, GCornerNone);
+  }
+  
+  if(s_battery_level >= 50){
+    if(s_battery_level == 50){
+      size = 0;
+    }else{
+      size = (int)(float)(((float)(s_battery_level % 25) / 25.0F) * (bounds.size.h));
+    }
+    
+    graphics_fill_rect(ctx, GRect((bounds.size.w-4), ((bounds.size.h - size + 2)), 2, size-4), 0, GCornerNone);
+    graphics_fill_rect(ctx, GRect(1, (bounds.size.h-4), (bounds.size.w-4), 2), 0, GCornerNone);
+  }
+  
+  if(s_battery_level > 25){
+    size = (int)(float)(((float)(s_battery_level % 25) / 25.0F) * (bounds.size.w));
+    graphics_fill_rect(ctx, GRect(2, bounds.size.h-4, size-4, 2), 0, GCornerNone);
+    
+    graphics_fill_rect(ctx, GRect(1, 1, 2, (bounds.size.h-3)), 0, GCornerNone);  
+  }else{
+    size = (int)(float)(((float)s_battery_level / 25.0F) * (bounds.size.h));
+    graphics_fill_rect(ctx, GRect(1, 1, 2, (size-4)), 0, GCornerNone);
+  }
+   
 }
 
 static void main_window_load(Window *window) {
@@ -19,11 +64,22 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  s_time_layer = text_layer_create(GRect(4, PBL_IF_ROUND_ELSE(58, 52), (bounds.size.w-8), 50));
+  s_battery_layer = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  s_battery_percentage = text_layer_create(GRect(bounds.size.w-28, 4, 20, 25));
+  
+  layer_set_update_proc(s_battery_layer, battery_update_proc);
+  layer_add_child(window_get_root_layer(window), s_battery_layer);
+  
+  text_layer_set_background_color(s_battery_percentage, GColorBlack);
+  text_layer_set_text_color(s_battery_percentage, GColorWhite);
+  //text_layer_set_font(s_battery_percentage, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  //text_layer_set_text_alignment(s_battery_percentage, GTextAlignmentCenter);
+  
+  layer_add_child(window_layer, text_layer_get_layer(s_battery_percentage));
+  
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
@@ -32,10 +88,21 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window){
   text_layer_destroy(s_time_layer);
+  layer_destroy(s_battery_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+}
+
+static void battery_callback(BatteryChargeState state) {
+  s_battery_level = state.charge_percent;
+  layer_mark_dirty(s_battery_layer);
+  
+  static char s_buffer[8];
+  snprintf(s_buffer,sizeof(s_buffer), "%d", s_battery_level);
+   
+  text_layer_set_text(s_battery_percentage, s_buffer);
 }
 
 static void init(){
@@ -47,11 +114,12 @@ static void init(){
   });
   
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  battery_state_service_subscribe(battery_callback);
 
   window_stack_push(s_main_window, true);
+  battery_callback(battery_state_service_peek());
   update_time();
 }
-
 
 static void deinit(){
   window_destroy(s_main_window);
