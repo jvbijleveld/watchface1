62aea33ec41bf8ec39dd456c9820560d5f42a578
Jeffrey <jeffrey@vanbijleveld.nl>
2016-06-02 16:16:08 +0200

    Weather by geo and disconnect warning

--- src/WatchFace1.c
+++ src/WatchFace1.c
@@ -10,13 +10,14 @@ static Layer *s_battery_layer;
 static TextLayer *s_battery_percentage;
 static TextLayer *s_date_layer;
 static TextLayer *s_weather_layer;
-
+static BitmapLayer *s_bt_icon_layer;
+static GBitmap *s_bt_icon_bitmap;
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Store incoming information
   static char temperature_buffer[8];
-  static char conditions_buffer[32];
-  static char weather_layer_buffer[32];
+  //static char conditions_buffer[32];
+  //static char weather_layer_buffer[32];
 
   APP_LOG(APP_LOG_LEVEL_DEBUG, "received Callback");
   
@@ -27,11 +28,20 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   // If all data is available, use it
   if(temp_tuple && conditions_tuple) {
     snprintf(temperature_buffer, sizeof(temperature_buffer), "%dc", (int)temp_tuple->value->int32);
-    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+    //snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+    //snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, temperature_buffer);
+  }
+}
 
-    // Assemble full string and display
-    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
-    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+static void bluetooth_callback(bool connected) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Got BT callback");
+  
+  layer_set_hidden(bitmap_layer_get_layer(s_bt_icon_layer), connected);
+
+  if(!connected) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "BT not connected");
+    vibes_double_pulse();
   }
 }
 
@@ -119,6 +129,7 @@ static void main_window_load(Window *window) {
   s_battery_percentage = text_layer_create(GRect(bounds.size.w-28, 4, 20, 25));
   s_date_layer = text_layer_create(GRect(4, 75, (bounds.size.w-8), 35));
   s_weather_layer = text_layer_create(GRect(4, 115, (bounds.size.w-8), 35));
+  s_bt_icon_layer = bitmap_layer_create(GRect(5, 5, 12, 16));
   
   layer_set_update_proc(s_battery_layer, battery_update_proc);
   layer_add_child(window_get_root_layer(window), s_battery_layer);
@@ -145,9 +156,17 @@ static void main_window_load(Window *window) {
   //temp layer
   text_layer_set_background_color(s_weather_layer, GColorBlack);
   text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_font(s_weather_layer, s_h2_font);
   text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
   text_layer_set_text(s_weather_layer, "..loading..");
   layer_add_child(window_layer, text_layer_get_layer(s_weather_layer));
+  
+  //connection layer
+  s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PHONE_CONNECTED);
+  bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_icon_layer));
+  
+  bluetooth_callback(connection_service_peek_pebble_app_connection());
 }
 
 static void main_window_unload(Window *window){
@@ -156,6 +175,8 @@ static void main_window_unload(Window *window){
   text_layer_destroy(s_weather_layer);
   text_layer_destroy(s_battery_percentage);
   layer_destroy(s_battery_layer);
+  gbitmap_destroy(s_bt_icon_bitmap);
+  bitmap_layer_destroy(s_bt_icon_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -166,10 +187,10 @@ static void battery_callback(BatteryChargeState state) {
   s_battery_level = state.charge_percent;
   layer_mark_dirty(s_battery_layer);
   
-  static char s_buffer[8];
-  snprintf(s_buffer,sizeof(s_buffer), "%d", s_battery_level);
+  //static char s_buffer[8];
+  //snprintf(s_buffer,sizeof(s_buffer), "%d", s_battery_level);
    
-  text_layer_set_text(s_battery_percentage, s_buffer);
+  //text_layer_set_text(s_battery_percentage, s_buffer);
 }
 
 static void init(){
@@ -179,20 +200,31 @@ static void init(){
     .load = main_window_load,
     .unload = main_window_unload
   });
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init::main_window_created");
   
   window_stack_push(s_main_window, true);
   
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init::updateTime");
   update_time();
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init::setBatteryState");
   battery_callback(battery_state_service_peek());
   
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init::Register Timer Ticker");
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init::Register Battery event");
   battery_state_service_subscribe(battery_callback);
   
-  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init::register AppMessage events");
   app_message_register_inbox_received(inbox_received_callback);
   app_message_register_inbox_dropped(inbox_dropped_callback);
   app_message_register_outbox_failed(outbox_failed_callback);
   app_message_register_outbox_sent(outbox_sent_callback);
+  
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init::register BT event");
+  connection_service_subscribe((ConnectionHandlers) {
+    .pebble_app_connection_handler = bluetooth_callback
+  });
   
   const int inbox_size = 128;
   const int outbox_size = 128;
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
-var APPID = 'dj0yJmk9ZW1iVzIxcENjamNqJmQ9WVdrOU9HUnRRVlJ1Tm5FbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lYQ--';
-var yahooUrl = 'https://query.yahooapis.com/v1/public/yql?q=<wsql>&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
-
+var APPID = 'd8be18b5726a69e2fc838d789b558850';
 
 var xhrRequest = function (url, type, callback) {
   var xhr = new XMLHttpRequest();
@@ -11,70 +9,29 @@ var xhrRequest = function (url, type, callback) {
   xhr.send();
 };
 
-
 function locationSuccess(pos) {
-  // Construct URL
-  //var query = 'select * from geo.places where text = "' + pos.coords.latitude + ',' + pos.coords.longitude + '"';
-  var query = 'select * from geo.places where text = "almere"';
- 
-  // Send request to OpenWeatherMap
-  xhrRequest(yahooUrl.replace('<wsql>',query), 'GET', function(responseText) {
-      // responseText contains a JSON object with weather info
-    console.log(yahooUrl.replace('<wsql>',query));
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + APPID;
+  xhrRequest(url, 'GET', function(responseText) {
     console.log(responseText);
-    
-    var json = JSON.parse(responseText);
-    console.log(json.query.results.place[0].woeid);
-    
-    var query = 'select * from weather.forecast where woeid ="' + json.query.results.place[0].woeid + '"';
-    xhrRequest(yahooUrl.replace('<wsql>',query), 'GET', function(responseText) {
-      console.log(responseText);
       
-      var weather = JSON.parse(responseText);
-      var temperature = Math.round((weather.query.results.channel.item.condition.temp - 32) * 5 / 9);
-      var conditions = weather.query.results.channel.item.condition.text;
+    var weather = JSON.parse(responseText);
+    var temperature = Math.round(weather.main.temp - 273.15);
+    var conditions = weather.weather[0].main;
       
-      console.log(temperature + " " + conditions);
-      var dictionary = {
+    console.log(temperature + " " + conditions);
+    var dictionary = {
         "KEY_TEMPERATURE": temperature,
         "KEY_CONDITIONS": conditions
-      };
+    };
       
-      Pebble.sendAppMessage(dictionary,
-        function(e) {
-          console.log('Weather info sent to Pebble successfully!');
-        },
-        function(e) {
-          console.log('Error sending weather info to Pebble!');
-        }
-      );
-      
-    });
-    
-  });
-
-}
-
-
-function getFixxedWeather(){
-  var wsql = 'select * from weather.forecast where woeid=WID and u="'+DEG+'"';
-  var url = "https://query.yahooapis.com/v1/public/yql?q=" + wsql + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-  
-  console.log(url);
-      
-  xhrRequest(url, 'GET', function(resp){
-    console.log(resp);
-    var weather = JSON.parse(resp);
-  	if(weather.query.count == 1){
-  	  var item = weather.query.results.channel.item.condition;
-         
-      var dictionary = {
-           "KEY_TEMPERATURE": item.temp,
-           "KEY_CONDITIONS": item.text
-      };
-  	} else {
-  	  console.log("Error retrieving weather data!");
-  	}
+    Pebble.sendAppMessage(dictionary,
+      function(e) {
+        console.log('Weather info sent to Pebble successfully!');
+      },
+      function(e) {
+        console.log('Error sending weather info to Pebble!');
+      }
+    );
   });
 }
 
